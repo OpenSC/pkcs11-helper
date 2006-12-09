a8fa5aaa2d81a0c168075bdbdb46722191615345
Alon Bar-Lev <alon.barlev@gmail.com>
2006-12-09 12:57:27 +0000

    signed to unsigned cleanups

--- tests/slotevent/slotevent.c
+++ tests/slotevent/slotevent.c
@@ -22,7 +22,7 @@ int main () {
 static
 void
 fatal (const char * const m, CK_RV rv) {
-	fprintf (stderr, "%s - %08lx - %s\n", m, rv, pkcs11h_getMessage (rv));
+	fprintf (stderr, "%s - %08lu - %s\n", m, rv, pkcs11h_getMessage (rv));
 	exit (1);
 }
 
